bdf9a58ca9fcb953525f04a2c916699813a0b65a
Fernando Mogno Volpini <139160471+FernandoVolpini@users.noreply.github.com>
2026-03-29 20:37:12 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ferna\\OneDrive\\Desktop\\Programacao\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ferna\\OneDrive\\Desktop\\Parcial\\parcial_devmob" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ferna\\OneDrive\\Desktop\\Programacao\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\ferna\\OneDrive\\Desktop\\Parcial\\parcial_devmob"
+  "FLUTTER_ROOT=C:\\Users\\ferna\\OneDrive\\Desktop\\Programacao\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ferna\\OneDrive\\Desktop\\Parcial\\parcial_devmob\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ferna\\OneDrive\\Desktop\\Parcial\\parcial_devmob"
+  "FLUTTER_TARGET=C:\\Users\\ferna\\OneDrive\\Desktop\\Parcial\\parcial_devmob\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmYzN2JlZjYwMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZTRiOGRjYTNmMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ferna\\OneDrive\\Desktop\\Parcial\\parcial_devmob\\.dart_tool\\package_config.json"
+)
